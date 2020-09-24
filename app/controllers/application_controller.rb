7a75e45b11778ec0d872fa2594103f8aed3face3
Arthur Messean <arthur.messean@gmail.com>
2020-09-24 14:45:49 +0200

    devise updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  after_action :verify_authorized, except: :index, unless: :devise_controller?
+  after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
 end
