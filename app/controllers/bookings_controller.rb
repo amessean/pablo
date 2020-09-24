51ad0c096623f5fddd550ca4b8cef1ed955398e0
Arthur Messean <arthur.messean@gmail.com>
2020-09-24 19:57:58 +0200

    crud in progress

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,5 +4,29 @@ class BookingsController < ApplicationController
   end
 
   def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+    redirect_to bookings_path
+  end
+
+private
+
+  def booking_params
+    params.require(:booking).permit(:title, :category, :user_id)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :users, optional: true
   has_many :reviews, dependent: :destroy
+  CATEGORY = ["Diner", "Party", "Weekend", "Holiday"]
 end
